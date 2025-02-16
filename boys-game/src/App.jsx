2dffca3af5e82b77ff8a368c3d3cb97609763b9a
Lavan Sumanan <lavan.sumanan@gmail.com>
2025-02-16 16:21:14 -0500

    add warning before reloads

--- boys-game/src/App.jsx
+++ boys-game/src/App.jsx
@@ -2,8 +2,22 @@ import { TeamsProvider } from "./contexts/TeamsContext.jsx";
 import { ScreenProvider } from "./contexts/ScreensContext.jsx";
 import { Screen } from "./components/Screen.jsx";
 import { BoysProvider } from "./contexts/BoysContext.jsx";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = "Are you sure you want to leave?"; // Standard for modern browsers
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <ScreenProvider>
       <TeamsProvider>
